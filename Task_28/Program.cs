0ed67c0318be414845838e5576492f884ef59c67
Andrey Bulgakov <leoncat19@gmail.com>
2022-07-15 09:54:12 +0300

    Add Task_46

---
+++ Task_28/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+int res = 1;
+int start = 1;
+
+while (start <= n)
+{
+    checked
+    {
+        res *= start; // res = res * start
+        start++;
+    }
+}
+
+Console.WriteLine(res);
---
+++ Task_30/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 30: Напишите программу, которая выводит
+// массив из 8 элементов, заполненный нулями и
+// единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int[] array = new int[8];
+int[] array1 = {3, 5, 8, 8};
+
+int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+
+int index = 0;
+var rnd = new Random();
+
+while (index < array.Length)
+{
+    array[index] = int.Parse(Console.ReadLine());
+    index++;
+}
+
+for (int i = 0; i < nums.Length; i++)
+{
+    if (i == 0) Console.Write("[");
+    if (i < nums.Length - 1) Console.Write(nums[i] + ",");
+    else Console.Write(nums[i] + "]");
+}
+
